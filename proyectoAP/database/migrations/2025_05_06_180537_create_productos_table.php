1e3c3de0f30904d938c3205e8dab9600a73196c6
Nereaaa <nereap492@gmail.com>
2025-05-06 20:35:17 +0200

    creacion de tablas y relaciones para la base de datos

---
+++ proyectoAP/database/migrations/2025_05_06_180537_create_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('foto')->nullable();
+            $table->string('medidas');
+            $table->string('color');
+            $table->decimal('precio', 10, 2)->nullable();
+            $table->foreignId('catalogo_id')->constrained('catalogos')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
---
+++ proyectoAP/database/migrations/2025_05_06_181747_create_gastos_fabricacion_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gastos_fabricacion', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->decimal('precio_hora', 10, 2)->nullable();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gastos_fabricacion');
+    }
+};
